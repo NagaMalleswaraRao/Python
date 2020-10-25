7b60cb5957070b5d30af43540ee3ae8c76c3d326
Naga Malleswara Rao Borra <nag.bn.1993@gmail.com>
2020-10-25 22:49:34 +0530

    Update Code_snippets.py

--- Code_snippets.py
+++ Code_snippets.py
@@ -2,3 +2,12 @@
 import numpy as np
 import pandas as pd
 
+#reading excel file (if it's in the same folder)
+file_name = 'Data.xlsx'
+pd.read_excel(file_name)
+
+#assigning data from excel file to a data frame
+file_name = 'Data.xlsx'
+df = pd.read_excel(file_name)
+print(df)
+
